2c58d3c6079e00c1312448895eae6a2328a06f53
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-12 20:17:22 -0600

    Clase creada

--- Actividad13/Program.cs
+++ Actividad13/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Dispositivos
+{
+    public string Nombre;
+    public double Rendimiento1;
+    public double Rendimiento2;
+    public double Rendimiento3;
+
+    public double CalcularPromedio()
+    { 
+        return (Rendimiento1 + Rendimiento2 + Rendimiento3) / 3;
+    }
+
+    public string Estado()
+    {
+        if (CalcularPromedio()>=80)
+        {
+            return "Óptimo";
+        }
+        else if (CalcularPromedio()<80)
+        {
+            return "Necesita mantenimiento";
+        }
+
+    }
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Nombre:{Nombre} | Promedio Rendimiento:{CalcularPromedio} | Estado:{Estado} ");
+    }
+
+}
