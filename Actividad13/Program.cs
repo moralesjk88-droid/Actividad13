b8d031c86808451f87490ee5621c1eeec291074f
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-12 20:25:03 -0600

    Creacion de lista

--- Actividad13/Program.cs
+++ Actividad13/Program.cs
@@ -1,5 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-class Dispositivos
+
+List<Dispositivo> dispositivos = new List<Dispositivo>();
+Console.WriteLine("¿Cúantos sispositivos desea registrar");
+int n=int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Dispositivo dis = new Dispositivo();
+    Console.WriteLine($"\nDispositivos{i + 1}");
+    Console.Write("Nombre del dispositivo:_ "); dis.Nombre = Console.ReadLine();
+    Console.Write("Resultado de la prueva 1:_ "); dis.Rendimiento1=double.Parse(Console.ReadLine());
+    Console.Write("Resultado de la prueva 2:_ "); dis.Rendimiento2 = double.Parse(Console.ReadLine());
+    Console.Write("Resultado de la prueva 3:_ "); dis.Rendimiento3 = double.Parse(Console.ReadLine());
+}
+
+
+
+class Dispositivo
 {
     public string Nombre;
     public double Rendimiento1;
